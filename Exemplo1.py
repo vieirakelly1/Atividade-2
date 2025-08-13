4a7d8d27f133adac503417a2c6a4deefc23f217d
vieirakelly1 <kellyvieira2208@gmail.com>
2025-08-13 17:34:44 -0300

    Add files via upload

---
+++ Exemplo1.py
@@ -0,0 +1,13 @@
+senha=input('digite a sua senha')
+
+senhaCorreta='casarosa'
+
+print(senha==senhaCorreta)
+
+# SE
+if senha==senhaCorreta:
+    print('acesso permitido')
+
+# SENAO
+else:
+    print('acesso negado')
---
+++ Exemplo2.py
@@ -0,0 +1,11 @@
+temperatura = float(input("Digite a temperatura em Celsius: "))
+
+if temperatura >= 30:
+    print("Está quente.")
+elif temperatura >= 20:
+    print("Está agradável.")
+elif temperatura >= 10:
+    print("Está frio")
+else:
+    print("Está muito frio!")
+    
