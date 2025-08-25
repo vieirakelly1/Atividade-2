8f89e26e7f115061858e84b01c2a81fa069d9e44
vieirakelly1 <kellyvieira2208@gmail.com>
2025-08-25 17:48:45 -0300

    Add files via upload

--- funcoes.py
+++ funcoes.py
@@ -7,3 +7,11 @@ def dividir(numero1, numero2):
 
 def escreva(frase):
     print(frase)
+
+def somult (n1, n2, n3):
+    return n1 + n2 * n3
+
+def identidade(nome, idade):
+    print(nome)
+    print(idade)
+    
